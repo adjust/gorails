e602c43f789b52643ad04a383da1f84fa5042471
Andrew Slotin <andrew.slotin@gmail.com>
2013-12-01 20:36:58 +0100

    Fixed negative integers loading

--- marshal/marshal.go
+++ marshal/marshal.go
@@ -1,94 +1,102 @@
 package marshal
 
 import (
-  "errors"
-  "log"
+	"errors"
 )
 
 type MarshalledObject struct {
-  MajorVersion byte
-  MinorVersion byte
-  data         []byte
+	MajorVersion byte
+	MinorVersion byte
+	data         []byte
 }
 
 type marshalledObjectType byte
-var TypeMismatch      = errors.New("gorails/marshal: an attempt to implicitly typecast the marshalled object")
+
+var TypeMismatch = errors.New("gorails/marshal: an attempt to implicitly typecast the marshalled object")
 
 const (
-  TYPE_UNKNOWN marshalledObjectType = 0
-  TYPE_NIL     marshalledObjectType = 1
-  TYPE_BOOLEAN marshalledObjectType = 2
-  TYPE_INTEGER marshalledObjectType = 3
-  TYPE_FLOAT   marshalledObjectType = 4
-  TYPE_ARRAY   marshalledObjectType = 5
-  TYPE_MAP     marshalledObjectType = 6
+	TYPE_UNKNOWN marshalledObjectType = 0
+	TYPE_NIL     marshalledObjectType = 1
+	TYPE_BOOLEAN marshalledObjectType = 2
+	TYPE_INTEGER marshalledObjectType = 3
+	TYPE_FLOAT   marshalledObjectType = 4
+	TYPE_ARRAY   marshalledObjectType = 5
+	TYPE_MAP     marshalledObjectType = 6
 )
 
-func CreateMarshalledObject(serialized_data []byte) (*MarshalledObject) {
-  return &(MarshalledObject{serialized_data[0], serialized_data[1], serialized_data[2:]})
+func CreateMarshalledObject(serialized_data []byte) *MarshalledObject {
+	return &(MarshalledObject{serialized_data[0], serialized_data[1], serialized_data[2:]})
 }
 
 func assertType(obj *MarshalledObject, expected_type marshalledObjectType) (err error) {
-  if obj.GetType() != expected_type {
-    err = TypeMismatch
-  }
+	if obj.GetType() != expected_type {
+		err = TypeMismatch
+	}
 
-  return
+	return
 }
 
 func (obj *MarshalledObject) GetType() (object_type marshalledObjectType) {
-  switch (obj.data[0]) {
-  case '0':
-    object_type = TYPE_NIL
-  case 'T', 'F':
-    object_type = TYPE_BOOLEAN
-  case 'i':
-    object_type = TYPE_INTEGER
-  case 'f':
-    object_type = TYPE_FLOAT
-  case '[':
-    object_type = TYPE_ARRAY
-  case '{':
-    object_type = TYPE_MAP
-  default:
-    object_type = TYPE_UNKNOWN
-  }
+	switch obj.data[0] {
+	case '0':
+		object_type = TYPE_NIL
+	case 'T', 'F':
+		object_type = TYPE_BOOLEAN
+	case 'i':
+		object_type = TYPE_INTEGER
+	case 'f':
+		object_type = TYPE_FLOAT
+	case '[':
+		object_type = TYPE_ARRAY
+	case '{':
+		object_type = TYPE_MAP
+	default:
+		object_type = TYPE_UNKNOWN
+	}
 
-  return
+	return
 }
 
 func (obj *MarshalledObject) GetAsBoolean() (value bool, err error) {
-  err = assertType(obj, TYPE_BOOLEAN)
-  if err == nil {
-    value = obj.data[0] == 'T'
-  }
+	err = assertType(obj, TYPE_BOOLEAN)
+	if err == nil {
+		value = obj.data[0] == 'T'
+	}
 
-  return
+	return
 }
 
 func (obj *MarshalledObject) GetAsInteger() (value int, err error) {
-  err = assertType(obj, TYPE_INTEGER)
-  if err != nil {
-    return
-  }
+	err = assertType(obj, TYPE_INTEGER)
+	if err != nil {
+		return
+	}
 
-  if obj.data[1] > 0x05 && obj.data[1] < 0xfb {
-    value = int(obj.data[1])
+	if obj.data[1] > 0x05 && obj.data[1] < 0xfb {
+		value = int(obj.data[1])
 
-    if value > 0x7f {
-      value = -(0xff ^ value + 1) + 5
-    } else {
-      value -= 5
-    }
-  } else {
-    value = 0
-    i := obj.data[1] & 0xf0
+		if value > 0x7f {
+			value = -(0xff ^ value + 1) + 5
+		} else {
+			value -= 5
+		}
+	} else if obj.data[1] <= 0x05 {
+		value = 0
+		i := obj.data[1]
 
-    log.Printf("Bytes num: %d", i)
-    for ; i > 0; i-- {
-      value = value << 8 + int(obj.data[i + 1] ^ 0xff)
-    }
-  }
+		for ; i > 0; i-- {
+			value = value<<8 + int(obj.data[i+1])
+		}
+	} else {
+		value = 0
+		i := 0xff - obj.data[1] + 1
 
-  return
+		for ; i > 0; i-- {
+			value = value<<8 + (0xff - int(obj.data[i+1]))
+		}
+
+		value = -(value + 1)
+	}
+
+	return
 }
--- marshal/marshal_test.go
+++ marshal/marshal_test.go
@@ -1,88 +1,125 @@
 package marshal
 
 import (
-  "testing"
+	"testing"
 )
 
 func TestCreateMarshalledObject(t *testing.T) {
-  m := CreateMarshalledObject([]byte{4, 8, 1})
+	m := CreateMarshalledObject([]byte{4, 8, 1})
 
-  if m.MajorVersion != 4 {
-    t.Errorf("CreateMarshalledObject created an object with Marshal major version set to %d instead of 4", m.MajorVersion)
-  }
+	if m.MajorVersion != 4 {
+		t.Errorf("CreateMarshalledObject created an object with Marshal major version set to %d instead of 4", m.MajorVersion)
+	}
 
-  if m.MinorVersion != 8 {
-    t.Errorf("CreateMarshalledObject created an object with Marshal minor version set to %d instead of 8", m.MinorVersion)
-  }
+	if m.MinorVersion != 8 {
+		t.Errorf("CreateMarshalledObject created an object with Marshal minor version set to %d instead of 8", m.MinorVersion)
+	}
 }
 
 type getTypeTestCase struct {
-  Data        []byte
-  Expectation marshalledObjectType
+	Data        []byte
+	Expectation marshalledObjectType
 }
 
 func TestGetType(t *testing.T) {
-  marshalledObjectTypeNames := []string{"unknown", "nil", "boolean", "integer", "float", "array", "map"}
+	marshalledObjectTypeNames := []string{"unknown", "nil", "boolean", "integer", "float", "array", "map"}
 
-  tests := []getTypeTestCase{
-    // Nil
-    {[]byte{4, 8, 48}, TYPE_NIL},
-    // Booleans
-    {[]byte{4, 8, 70}, TYPE_BOOLEAN}, // false
-    {[]byte{4, 8, 84}, TYPE_BOOLEAN}, // true
-    // Integers
-    {[]byte{4, 8, 105, 0}, TYPE_INTEGER}, // 0
-    {[]byte{4, 8, 105, 6}, TYPE_INTEGER}, // 1
-    {[]byte{4, 8, 105, 250}, TYPE_INTEGER}, // -1
-    {[]byte{4, 8, 105, 3, 64, 226, 1}, TYPE_INTEGER}, // 123456
-    {[]byte{4, 8, 105, 253, 192, 29, 254}, TYPE_INTEGER}, // -123456
-    // Floats
-    {[]byte{4, 8, 102, 6, 48}, TYPE_FLOAT}, // 0.0
-    {[]byte{4, 8, 102, 8, 49, 46, 53}, TYPE_FLOAT}, // 1.5
-    {[]byte{4, 8, 102, 9, 45, 49, 46, 53}, TYPE_FLOAT}, // -1.5
-    {[]byte{4, 8, 102, 12, 49, 46, 50, 53, 101, 51, 48}, TYPE_FLOAT}, // 1.25e30
-    {[]byte{4, 8, 102, 13, 49, 46, 50, 53, 101, 45, 51, 48}, TYPE_FLOAT}, // 1.25e-30
-    // Arrays
-    {[]byte{4, 8, 91, 0}, TYPE_ARRAY}, // []
-    {[]byte{4, 8, 91, 6, 73, 34, 8, 102, 111, 111, 6, 58, 6, 69, 84}, TYPE_ARRAY}, // ["foo"]
-    // Maps (Ruby hashes)
-    {[]byte{4, 8, 123, 0}, TYPE_MAP}, // {}
-    {[]byte{4, 8, 123, 6, 58, 8, 102, 111, 111, 73, 34, 8, 98, 97, 114, 6, 58, 6, 69, 84}, TYPE_MAP}, // {foo: "bar"}
-  }
+	tests := []getTypeTestCase{
+		// Nil
+		{[]byte{4, 8, 48}, TYPE_NIL},
+		// Booleans
+		{[]byte{4, 8, 70}, TYPE_BOOLEAN}, // false
+		{[]byte{4, 8, 84}, TYPE_BOOLEAN}, // true
+		// Integers
+		{[]byte{4, 8, 105, 0}, TYPE_INTEGER},                 // 0
+		{[]byte{4, 8, 105, 6}, TYPE_INTEGER},                 // 1
+		{[]byte{4, 8, 105, 250}, TYPE_INTEGER},               // -1
+		{[]byte{4, 8, 105, 3, 64, 226, 1}, TYPE_INTEGER},     // 123456
+		{[]byte{4, 8, 105, 253, 192, 29, 254}, TYPE_INTEGER}, // -123456
+		// Floats
+		{[]byte{4, 8, 102, 6, 48}, TYPE_FLOAT},                               // 0.0
+		{[]byte{4, 8, 102, 8, 49, 46, 53}, TYPE_FLOAT},                       // 1.5
+		{[]byte{4, 8, 102, 9, 45, 49, 46, 53}, TYPE_FLOAT},                   // -1.5
+		{[]byte{4, 8, 102, 12, 49, 46, 50, 53, 101, 51, 48}, TYPE_FLOAT},     // 1.25e30
+		{[]byte{4, 8, 102, 13, 49, 46, 50, 53, 101, 45, 51, 48}, TYPE_FLOAT}, // 1.25e-30
+		// Arrays
+		{[]byte{4, 8, 91, 0}, TYPE_ARRAY},                                             // []
+		{[]byte{4, 8, 91, 6, 73, 34, 8, 102, 111, 111, 6, 58, 6, 69, 84}, TYPE_ARRAY}, // ["foo"]
+		// Maps (Ruby hashes)
+		{[]byte{4, 8, 123, 0}, TYPE_MAP},                                                                 // {}
+		{[]byte{4, 8, 123, 6, 58, 8, 102, 111, 111, 73, 34, 8, 98, 97, 114, 6, 58, 6, 69, 84}, TYPE_MAP}, // {foo: "bar"}
+	}
 
-  for _, testCase := range tests {
-    object_type := CreateMarshalledObject(testCase.Data).GetType()
-    if object_type != testCase.Expectation {
-      t.Errorf("GetType() returned '%s' instead of '%s'", marshalledObjectTypeNames[int(object_type)], marshalledObjectTypeNames[testCase.Expectation])
-    }
-  }
+	for _, testCase := range tests {
+		object_type := CreateMarshalledObject(testCase.Data).GetType()
+		if object_type != testCase.Expectation {
+			t.Errorf("GetType() returned '%s' instead of '%s'", marshalledObjectTypeNames[int(object_type)], marshalledObjectTypeNames[testCase.Expectation])
+		}
+	}
 }
 
 type getAsBooleanTestCase struct {
-  Data        []byte
-  Expectation bool
+	Data        []byte
+	Expectation bool
 }
 
 func TestGetAsBoolean(t *testing.T) {
-  tests := []getAsBooleanTestCase{
-    {[]byte{4, 8, 70}, false},
-    {[]byte{4, 8, 84}, true},
-  }
+	tests := []getAsBooleanTestCase{
+		{[]byte{4, 8, 70}, false},
+		{[]byte{4, 8, 84}, true},
+	}
 
-  value, err := CreateMarshalledObject([]byte{4, 8, 48}).GetAsBoolean() // should return an error
-  if err == nil {
-    t.Error("GetAsBoolean() returned no error when attempted to typecast nil to boolean")
-  }
+	value, err := CreateMarshalledObject([]byte{4, 8, 48}).GetAsBoolean() // should return an error
+	if err == nil {
+		t.Error("GetAsBoolean() returned no error when attempted to typecast nil to boolean")
+	}
 
-  for _, testCase := range tests {
-    value, err = CreateMarshalledObject(testCase.Data).GetAsBoolean()
+	for _, testCase := range tests {
+		value, err = CreateMarshalledObject(testCase.Data).GetAsBoolean()
 
-    if err != nil {
-      t.Errorf("GetAsBoolean() returned an error: '%s' for %t", err.Error(), testCase.Expectation)
-    }
+		if err != nil {
+			t.Errorf("GetAsBoolean() returned an error: '%s' for %t", err.Error(), testCase.Expectation)
+		}
 
-    if value != testCase.Expectation {
-      t.Errorf("GetAsBoolean() returned '%t' instead of '%t'", value, testCase.Expectation)
-    }
-  }
+		if value != testCase.Expectation {
+			t.Errorf("GetAsBoolean() returned '%t' instead of '%t'", value, testCase.Expectation)
+		}
+	}
+}
+
+type getAsIntegerTestCase struct {
+	Data        []byte
+	Expectation int
+}
+
+func TestGetAsInteger(t *testing.T) {
+	tests := []getAsIntegerTestCase{
+		{[]byte{4, 8, 0x69, 0x00}, 0},
+		{[]byte{4, 8, 0x69, 0x06}, 1},
+		{[]byte{4, 8, 0x69, 0x7f}, 122},
+		{[]byte{4, 8, 0x69, 0x01, 0x7b}, 123},
+		{[]byte{4, 8, 0x69, 0x02, 0x00, 0x01}, 256},
+		{[]byte{4, 8, 0x69, 0x04, 0xff, 0xff, 0xff, 0x3f}, (2 << 29) - 1},
+		{[]byte{4, 8, 0x69, 0xfa}, -1},
+		{[]byte{4, 8, 0x69, 0xff, 0x84}, -124},
+		{[]byte{4, 8, 0x69, 0xfe, 0xff, 0xfe}, -257},
+		{[]byte{4, 8, 0x69, 0xfc, 0x00, 0x00, 0x00, 0xc0}, -(2 << 29)},
+	}
+
+	value, err := CreateMarshalledObject([]byte{4, 8, 48}).GetAsInteger() // should return an error
+	if err == nil {
+		t.Error("GetAsInteger() returned no error when attempted to typecast nil to boolean")
+	}
+
+	for _, testCase := range tests {
+		value, err = CreateMarshalledObject(testCase.Data).GetAsInteger()
+
+		if err != nil {
+			t.Errorf("GetAsInteger() returned an error: '%s' for %d", err.Error(), testCase.Expectation)
+		}
+
+		if value != testCase.Expectation {
+			t.Errorf("GetAsInteger() returned '%d' instead of '%d'", value, testCase.Expectation)
+		}
+	}
 }
